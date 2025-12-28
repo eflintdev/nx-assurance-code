9d57bb52536d4ca25bf69201dd43ac5e08f940fd
Emmanuel C. Flint <emmanuelflint.dev@gmail.com>
2025-12-27 23:42:09 -0500

    feat: added a new banner component

---
+++ apps/frontend-stenciljs/src/components/banner-section/banner-section.tsx
@@ -0,0 +1,23 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'banner-section',
+  styleUrl: 'banner-section.scss',
+  shadow: true,
+})
+export class BannerSection {
+  /**
+   * Banner heading text
+   */
+  @Prop() heading!: string;
+
+  render() {
+    return (
+      <section class="banner-section">
+        <div class="banner-container">
+          <h1>{this.heading}</h1>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ apps/frontend-stenciljs/src/components/banner-section/readme.md
@@ -0,0 +1,17 @@
+# banner-section
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property               | Attribute | Description         | Type     | Default     |
+| ---------------------- | --------- | ------------------- | -------- | ----------- |
+| `heading` _(required)_ | `heading` | Banner heading text | `string` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
