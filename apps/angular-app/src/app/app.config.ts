4b216713f3fe7f5ca574e1690d8103f9492896af
Emmanuel C. Flint <emmanuelflint.dev@gmail.com>
2026-01-02 11:10:06 -0500

    chore: removed commented out code

--- apps/angular-app/src/app/app.config.ts
+++ apps/angular-app/src/app/app.config.ts
@@ -2,14 +2,9 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { DIRECTIVES } from './components/custom/dist/angular-directives';
-// import {
-//   provideClientHydration,
-//   withEventReplay,
-// } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes),
     ...DIRECTIVES
